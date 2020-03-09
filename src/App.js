348fdbed74d77dbd4f5d002581b0c291b9e93046
Miron Banks <madapegarage@gmail.com>
2020-03-09 02:51:48 +0100

    gsap animation

--- src/App.js
+++ src/App.js
@@ -29,10 +29,28 @@ function App() {
       stagger: {
         amount: 0.3
       }
-
-    })
-
-
+    }).to('.overlay-top', 1.6, {
+      height: 0,
+      ease: 'expo.inOut',
+      stagger: {
+        amount: 0.4
+      }
+    }).to('.overlay-bottom', 1.6, {
+      width: 0,
+      ease: 'expo.inOut',
+      delay: -0.8,
+      stagger: {
+        amount: 0.4
+      }
+    }).to('.intro-overlay', 0, { css: { display: "none" } })
+      .from('.case-image img', 1.6, {
+        scale: 1.4,
+        ease: 'expo.inOut',
+        delay: -2,
+        stagger: {
+          amount: 0.4
+        }
+      })
 
   }, [])
 
