b10c936292b59d1ef528657a52897cdd2839d411
Miron Banks <madapegarage@gmail.com>
2020-03-09 22:55:10 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { useEffect } from "react";
+import { Route } from 'react-router-dom'
 import "./styles/App.scss";
 import Header from "./components/header"
 import gsap from 'gsap'
+
+
+//pages
 import Home from "./pages/home";
+import CaseStudies from './pages/caseStudies'
+import Approach from './pages/approach'
+import About from './pages/about'
+import Services from './pages/services'
+
+
+const routes = [
+  { path: "/", name: 'Home', Component: Home },
+  { path: "/case-studies", name: 'Case Studies', Component: CaseStudies },
+  { path: "/approach", name: 'Approach', Component: Approach },
+  { path: "/about-us", name: 'About Us', Component: About },
+  { path: "/services", name: 'Services', Component: Services }
+]
 
 
 
@@ -20,7 +37,13 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            <Component />
+          </Route>
+        ))}
+      </div>
     </>
   );
 }
