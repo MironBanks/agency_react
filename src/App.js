0206a481f885fa3f1e1aef9f99dd5532eaf7322f
Miron Banks <madapegarage@gmail.com>
2020-03-09 02:23:36 +0100

    gsap for h2 title

--- src/App.js
+++ src/App.js
@@ -4,12 +4,36 @@ import Header from "./components/header"
 import Banner from "./components/banner";
 import Cases from "./components/cases";
 import IntroOverlay from "./components/introOverlay";
+import gsap from 'gsap'
+
+
+
 
 function App() {
 
   useEffect(() => {
     let vh = window.innerHeight * .01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+    //prevents flashing
+    gsap.to('body', 0, { css: { visibility: 'visible' } })
+
+
+    //timeline
+    const tl = gsap.timeline()
+
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3
+      }
+
+    })
+
+
+
   }, [])
 
 
