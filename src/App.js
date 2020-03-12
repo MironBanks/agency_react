58f6653f182392d53feb81e2d3c2abe5667d54e6
Miron Banks <madapegarage@gmail.com>
2020-03-12 01:28:35 +0100

    hamburger menu animation

--- src/App.js
+++ src/App.js
@@ -23,37 +23,51 @@ const routes = [
 ]
 
 
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments)
+    }, ms)
+  }
+}
+
 
 
 function App() {
+  gsap.to('body', 0, { css: { visibility: 'visible' } })
   const [dimensions, setDimensions] = useState({
     height: window.innerHeight,
     width: window.innerWidth
   })
   useEffect(() => {
 
-    let vh = window.innerHeight * .01
+    let vh = dimensions.height * .01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 
-    const HandleResize = () => {
+
+    const debouncedHandleResize = debounce(function handleResize() {
       setDimensions({
         height: window.innerHeight,
         width: window.innerWidth
       })
+    }, 1000)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
     }
-    window.addEventListener('resize', HandleResize)
 
-
-
-
-    gsap.to('body', 0, { css: { visibility: 'visible' } })
   })
 
 
   return (
     <>
-      <Header />
+      <Header dimensions={dimensions} />
       <div className="App">
         {routes.map(({ path, Component }) => (
           <Route key={path} exact path={path}>
--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,147 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+import { ReactComponent as UpArrow } from '../assets/up-arrow-circle.svg'
+import gsap from 'gsap'
 
-const Header = () => {
+
+
+
+let tl = gsap.timeline()
+
+
+
+
+const Header = ({ history, dimensions }) => {
+    const [menuState, setMenuState] = useState({ menuOpened: false })
+
+    useEffect(() => {
+
+        history.listen(() => {
+            setMenuState({ menuOpened: false })
+        })
+
+
+
+        if (menuState.menuOpened === true) {
+            gsap.to('nav', { css: { display: 'block' } })
+            gsap.to('body', { css: { overflow: 'hidden' } })
+
+            tl.to(".App", {
+                duration: 1,
+                y: dimensions.width <= 654 ? '85vh' : dimensions.height / 2,
+                ease: "expo.inOut"
+            }).to('.hamburger-menu span', {
+                duration: .6,
+                delay: -1,
+                scaleX: 0,
+                transformOrigin: '50% 0%',
+                ease: 'expo.inOut'
+            }).to('#Path_1', {
+                duration: 0.4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 5
+                }
+            }).to('#Path_2', {
+                duration: 0.4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 20
+                }
+            }).to('#Line_1', {
+                duration: 0.4,
+                delay: -0.6,
+                css: {
+                    strokeDashoffset: 40,
+                    strokeDasharray: 18
+                }
+            }).to('#circle', {
+                duration: 0.6,
+                delay: -0.8,
+                css: {
+                    strokeDashoffset: 0
+                }
+            }).to('.hamburger-menu-close', {
+                duration: 0.6,
+                delay: -0.8,
+                css: {
+                    display: "block"
+                }
+            })
+        } else {
+            tl.to('.App', {
+                duration: 1,
+                y: 0,
+                ease: 'expo.inOut'
+            }).to('#circle', {
+                duration: .6,
+                delay: -.6,
+                css: {
+                    strokeDashoffset: -193,
+                    strokeDasharray: 227
+                }
+            }).to('#Path_1', {
+                duration: .4,
+                delay: -.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 10
+                }
+            }).to('#Path_2', {
+                duration: .4,
+                delay: -.6,
+                css: {
+                    strokeDashoffset: 10,
+                    strokeDasharray: 10
+                }
+            }).to('#Line_1', {
+                duration: .4,
+                delay: -.6,
+                css: {
+                    strokeDashoffset: 40,
+                    strokeDasharray: 40
+                }
+            }).to('.hamburger-menu span', {
+                duration: .6,
+                delay: -.6,
+                scaleX: 1,
+                transformOrigin: '50% 0%',
+                ease: 'expo.inOut'
+            }).to('.hamburger-menu-close', {
+                css: {
+                    display: "none"
+                }
+            }).to('.body', {
+                css: {
+                    overflow: "auto"
+                }
+            }).to('.nav', {
+                css: {
+                    display: "none"
+                }
+            })
+        }
+    }, [dimensions.height, dimensions.width, history, menuState.menuOpened])
+
+
     return (
         <div className="header">
             <div className="container">
                 <div className="row v-center space-between">
                     <div className="logo">
-                        <a href="/">AGENCY.</a>
+                        <NavLink to='/'>AGENCY.</NavLink>
                     </div>
-                    <div className="nav">
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                    <div className="nav-toggle">
+                        <div onClick={() => setMenuState({ menuOpened: true })} className="hamburger-menu">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div onClick={() => setMenuState({ menuOpened: false })} className="hamburger-menu-close">
+                            <UpArrow />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -19,4 +149,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default withRouter(Header)
